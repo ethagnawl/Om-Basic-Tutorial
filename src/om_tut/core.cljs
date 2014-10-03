a781171857c6eb216e1e53cbab9bda12fad05259
Pete Doherty <pdoherty926@gmail.com>
2014-10-03 12:26:14 -0400

    wrapping up the basic tutorial

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -1,70 +1,127 @@
 (ns om-tut.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]
-            [clojure.data :as data]
             [clojure.string :as string]))
 
 (enable-console-print!)
 
 (def app-state
   (atom
-    {:beers
-      [ {:name "Road 2 Ruin" :ibu 99 :abv 4.0 :brewery "Two Roads"}
-        {:name "Roadsmary's Baby" :ibu 69 :abv 5.0 :brewery "Two Roads"}]}))
+    {:people
+     [{:type :student :first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:type :student :first "Alyssa" :middle-initial "P" :last "Hacker"
+       :email "aphacker@mit.edu"}
+      {:type :professor :first "Gerald" :middle "Jay" :last "Sussman"
+       :email "metacirc@mit.edu" :classes [:6001 :6946]}
+      {:type :student :first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:type :student :first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:type :professor :first "Hal" :last "Abelson" :email "evalapply@mit.edu"
+       :classes [:6001]}]
+     :classes
+     {:6001 "The Structure and Interpretation of Computer Programs"
+      :6946 "The Structure and Interpretation of Classical Mechanics"
+      :1806 "Linear Algebra"}}))
 
-(defn display-name [{:keys [:name :brewery] :as beer}]
-  (str name " (" brewery ")"))
+(extend-type string
+  ICloneable
+  (-clone [s] (js/String. s)))
 
-(defn parse-beer [beer-str]
-  (let [[name brewery abv ibu :as parts] (string/split beer-str #":")]
-        {:name name :brewery brewery :abv abv :ibu ibu}))
+(extend-type js/String
+  ICloneable
+  (-clone [s] (js/String. s))
+  om/IValue
+  (-value [s] (str s)))
 
-(defn add-beer [app owner]
-  (let [new-beer (-> (om/get-node owner "new-beer") .-value parse-beer)]
-    (when new-beer
-      (om/transact! app :beers #(conj % new-beer))
-      (om/set-state! owner :text ""))))
-
-(defn beer-view [beer owner]
-  (reify
-    om/IRenderState
-      (render-state [this {:keys [delete]}]
-        (dom/li nil
-          (dom/span nil (display-name beer))
-            (dom/button #js {:onClick (fn [e] (put! delete @beer))} "Delete")))))
-
-(defn handle-change [e owner {:keys [text]}]
-  (om/set-state! owner :text (.. e -target -value)))
-
-(defn beers-view [app owner]
+(defn editable [text owner]
   (reify
     om/IInitState
     (init-state [_]
-      {:delete (chan)
-       :text ""})
-    om/IWillMount
-    (will-mount [_]
-      (let [delete (om/get-state owner :delete)]
-        (go (loop []
-          (let [beer (<! delete)]
-            (om/transact! app :beers
-              (fn [xs] (vec (remove #(= beer %) xs))))
-            (recur))))))
+      {:editing false})
     om/IRenderState
-    (render-state [this state]
-      (dom/div nil
-        (dom/h2 nil "beer list")
-        (apply dom/ul nil
-          (om/build-all beer-view (:beers app)
-            {:init-state state}))
-        (dom/div nil
-          (dom/input #js {:js "text" :ref "new-beer" :value (:text state)
-                          :onChange #(handle-change % owner state)})
-          (dom/button #js {:onClick #(add-beer app owner)} "Add Beer"))))))
+    (render-state [_ {:keys [editing]}]
+      (dom/li nil
+        (dom/span #js {:style (display (not editing))} (om/value text))
+        (dom/input
+          #js {:style (display editing)
+               :value (om/value text)
+               :onChange #(handle-change % text owner)
+               :onKeyDown #(when (= (.-key %) "Enter")
+                              (commit-change text owner))
+               :onBlur (fn [e] (commit-change text owner))})
+        (dom/button
+          #js {:style (display (not editing))
+               :onClick #(om/set-state! owner :editing true)}
+          "Edit")))))
 
-(om/root
-  beers-view
-  app-state
-  {:target (. js/document (getElementById "beers"))})
+(defn display [show]
+  (if show
+    #js {}
+    #js {:display "none"}))
+
+(defn handle-change [e text owner]
+  (om/transact! text (fn [_] (.. e -target -value))))
+
+(defn commit-change [text owner]
+  (om/set-state! owner :editing false))
+
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
+
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn student-view [student owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/li nil (display-name student)))))
+
+(defn professor-view [professor owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/li nil
+        (dom/div nil (display-name professor))
+        (dom/label nil "Classes")
+        (apply dom/ul nil
+          (om/build-all editable (:classes professor)))))))
+
+(defmulti entry-view (fn [person _] (:type person)))
+
+(defmethod entry-view :student [person owner] (student-view person owner))
+
+(defmethod entry-view :professor [person owner] (professor-view person owner))
+
+(defn people [app]
+  (->> (:people app)
+    (mapv (fn [x]
+            (if (:classes x)
+              (update-in x [:classes]
+                (fn [cs] (mapv (:classes app) cs)))
+               x)))))
+
+(defn registry-view [app owner]
+  (reify
+    om/IRenderState
+    (render-state [_ state]
+      (dom/div nil
+        (dom/h2 nil "Registry")
+          (apply dom/ul nil
+            (om/build-all entry-view (people app)))))))
+
+(defn classes-view [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:id "classes"}
+        (dom/h2 nil "Classes")
+        (apply dom/ul nil
+          (om/build-all editable (vals (:classes app))))))))
+
+(om/root registry-view app-state
+  {:target (. js/document (getElementById "registry"))})
+
+(om/root classes-view app-state
+  {:target (. js/document (getElementById "classes"))})
